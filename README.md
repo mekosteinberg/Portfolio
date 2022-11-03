f4fe49baf2b166fd06ab25196dd747373fef6c2c
Meko Steinberg <steinberg.meko@outlook.com>
2022-11-02 21:06:26 -0700

    Portfolio 2.1

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Day 1: Tuesday 7am-2pmPST + evening/night work time;
     I've started out creating a base skeleton in my html, to get a feel for what I need to have laid out. I know I can (and should) use JQ to create some of these HTML elements, and I will probably amend that later, but Im very visual and need to see it first, even a bad visual helps my brain create a mental map of what needs to be done. Next step will be some styling and flexbox, then onto the JS/JQ. 
 Day 2: Part 1: Wednesday 6-2pm Stuggled to get a fade/expand feature working
      most the day, modified to a little different idea with Toggle Class than the original I sought out. Finally got one working, realized I should commit. Next step, get each main div working with the toggle, and then focus on modals for each resume in the Work Experience section.
+    Part 2: Wednesday evening. I realized I hadn't created multiple pages, just 
+        included everything within the scope of one page. SO that freaked me out and set me back. Created more pages to accomplish my tasks and set to work. Youtube more modal videos as recommended by mentors. 
--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const sosJobDescription = "Accomplish a broad range of responsibilities including; accurate and timely data entry, understanding, and selecting inventory and services in key databases, communicating with referral sources, and appropriately utilizing technology to notate patient information/communication. Have a thorough understanding the entire front-end process to ensure successful service for our patients. Works in a fast-paced environment answering inbound calls and making outbound calls. Maybe responsible for obtaining, analyze, and verify the accuracy of information received from referrals, create orders, and or schedule the patient to receive equipment as ordered by their doctor. Educate Patients of their financial responsibility when applicable. Responsible for ensuring customers receive PAP equipment by scheduling an appointment for the customer to come in to pick up equipment and receive proper instruction on how to best use the equipment. Responsible for encouraging client compliance with PAP equipment and program."
 
 const soundOxygen = () => {
-    const soundOx = $('<p>').attr('id', 'sO2 details');
-    $('#soundOx').text($(sosJobDescription)).appendTo($('soundO2')
-    )
+    $('<p>').attr('id', 'sO2JobDetails').text($(sosJobDescription)).appendTo($('#soundO2'))
 }
 
 
@@ -12,7 +10,22 @@ $(() => {
         $('.third > div.content').toggleClass("show");
     })
 
-    $('sO2').on('click', soundOxygen);
+    $('#so2Header').on('click', function () {
+        $('.third > div.content').toggleClass("show");
+    })
+
+    $('#fircrestHeader').on('click', function () {
+        $('.third > div.content').toggleClass("show");
+    })
+
+    $('#pumpwellHeader').on('click', function () {
+        $('.third > div.content').toggleClass("show");
+    })
+
+
+
+
+    //$('#soundO2').on('click', soundOxygen);
 
     // $('article').on('click', () {
     //     $('small').toggleClass('show-description'); })
